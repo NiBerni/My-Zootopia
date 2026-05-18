779340384f9af842c5f7c9ea87023102dd0bc76d
Berni <nickelb1988@gmail.com>
2026-05-18 18:32:14 +0200

    feat: add JSON data loader function to animals_web_generator.py
    - Implement `load_data` function to parse JSON file.
    - Load `animals_data.json` using the newly added function.
    
    Signed-off-by: Berni <nickelb1988@gmail.com>

---
+++ animals_web_generator.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+import json
+
+
+def load_data(file_path):
+	"""
+
+	:param file_path:
+	:return:
+	"""
+	with open(file_path, "r") as handle:
+		return json.load(handle)
+
+
+animals_data = load_data("animals_data.json")
